4a06bd5325a229c38e5426d38b8f7b749bcba495
jwong1219 <jwong1219@gmail.com>
2017-10-26 22:25:09 -0700

    apiRoutes now correctly pushes new user object into quizData for later uses

--- data/quizData.js
+++ data/quizData.js
@@ -1,13 +1,13 @@
 var answArr = [
-  {name: "Justin",
-  image: "",
-  results: [3,3,3,3,3,3,3,3,3,3]},
   {name: "Sandra",
   image: "",
-  results: [1,5,3,1,5,3,1,5,3,1]},
+  answers: ["1","5","3","1","5","3","1","5","3","1"]},
   {name: "Moose",
   image: "",
-  results: [5,3,1,5,3,1,5,3,1,5]}
+  answers: ["2","5","4","2","2","1","2","5","1","5"]},
+  {name: "Justin",
+  image: "",
+  answers: ["5","3","4","1","3","2","4","1","1","4"]},
 ];
 
 module.exports = answArr;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,31 @@
-var answArr = require("../data/quizData");
+var quizData = require("../data/quizData");
 
 function compareQuizzes(app) {
   app.get("/api/results", function(req, res) {
-    res.json(answArr);
+    res.json(quizData);
   });
 
   app.post("/api/results", function(request, response) {
-    var arr2 = request.body["answers[]"];
-    // console.log(parseInt(arr2[0]));
 
     var closest = null;
     var closestDiff = null;
 
-    //for each set of answers already in data
-    for (index1 in answArr) {
-      var sum = 0;
-      var times = 0;
-      //for each question in this set, add the results to the sum
-      for (index2 in answArr[index1].results) {
+    //turn the request into a user object that can be evaluated and then pushed to quizData
+    var newUser = {}
+    newUser.name = request.body.name;
+    newUser.image = request.body.image;
+    newUser.answers = request.body["answers[]"];
 
-        sum += Math.abs(parseInt(answArr[index1].results[index2]) - parseInt(arr2[index2]));
-        times++;
+    //for each set of answers already in data
+    for (index1 in quizData) {
+      var sum = 0;
+  
+      //for each question in this set, add the results to the sum
+      for (index2 in quizData[index1].answers) {
+        // console.log("data: " + quizData[index1].answers[index2]);
+        // console.log("user: " + newUser.answers[index2]);
+        sum += Math.abs(parseInt(quizData[index1].answers[index2]) - parseInt(newUser.answers[index2]));
+        console.log({sum});
       }
       //if there is not already a closest match, this must be the closest
       if(!closest){
@@ -33,9 +38,15 @@ function compareQuizzes(app) {
           closest = index1; //set "closest" to the index with the closest matched result.
           closestDiff = sum;
         }
-      } 
+      }
+      console.log(quizData[index1].name);
+      console.log({sum});
+      console.log(quizData[closest].name); 
+      console.log({closestDiff});
+      console.log("____________________");
     }
-    response.json(answArr[closest]);
+    quizData.push(newUser);
+    response.json(quizData[closest]);
   });
 }
 
